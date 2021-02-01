3da76e88e4d36d1891cb95e967570359be91677b
Mumtaz <mumtaz.maqsood@yahoo.com>
2021-02-01 23:11:33 +0100

    test

--- src/App.test.js
+++ src/App.test.js
@@ -20,3 +20,10 @@ test('renders learn react link1', () => {
   const linkElement = screen.getByText(/test/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('renders learn react link1', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/test1/i);
+  expect(linkElement).toBeInTheDocument();
+});
