b042cd422ca0c08357d4714c9220b95becaba659
Mumtaz <mumtaz.maqsood@yahoo.com>
2021-02-01 22:48:27 +0100

    add travis added

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Development/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('renders learn react link1', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Driven/i);
   expect(linkElement).toBeInTheDocument();
 });
